6658841c038d1bec53313a6385396dde597a636c
blackdreamer15 <bakijessy@gmail.com>
2024-07-06 16:34:54 +0000

    chore: Update Astro configuration with new integrations and options
    This commit updates the Astro configuration in the `astro.config.mjs` file. It adds new integrations such as `remark-toc`, `sitemap`, `partytown`, and `astro-expressive-code`. It also includes additional options for the `tailwind` integration and the `expressiveCode` plugin. These changes improve the codebase by enhancing the functionality and capabilities of the Astro project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,48 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import remarkToc from "remark-toc";
+import sitemap from "@astrojs/sitemap";
+import { SITE } from "./src/config";
+import partytown from "@astrojs/partytown";
+
+import expressiveCode from "astro-expressive-code";
+
+/** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
+const astroExpressiveCodeOptions = {
+  // Example: Change the theme to "dracula"
+  theme: "dracula",
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: SITE.website,
+  experimental: {
+    viewTransitions: true,
+    assets: true,
+  },
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    expressiveCode(astroExpressiveCodeOptions),
+  ],
+  markdown: {
+    remarkPlugins: [remarkToc],
+    extendDefaultPlugins: true,
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
 });
